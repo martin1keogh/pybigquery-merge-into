823aa119d7721ea831bc79a6f409877b772546a9
Martin Keogh <martin1keogh@gmail.com>
2021-04-11 01:25:36 +0200

    test: implement some of the SQL doc examples in python
    See https://cloud.google.com/bigquery/docs/reference/standard-sql/dml-syntax#merge_examples for the list of examples.
    
    Not all have been done yet.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import logging
 
-from sqlalchemy import Column, Date, MetaData, String, Table
+from sqlalchemy import ARRAY, Boolean, Column, Date, Integer, JSON, MetaData, String, Table
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -19,4 +19,38 @@ source = Table(
     metadata,
     Column("s1", String),
     Column("s2", Date)
+)
+
+# From https://cloud.google.com/bigquery/docs/reference/standard-sql/dml-syntax#merge_examples
+inventory = Table(
+    "dataset.Inventory",
+    metadata,
+    Column("product", String),
+    Column("quantity", Integer),
+    Column("supply_constrained", Boolean),
+)
+
+new_arrivals = Table(
+    "dataset.NewArrivals",
+    metadata,
+    Column("product", String),
+    Column("quantity", Integer),
+    Column("warehouse", String),
+)
+
+warehouse = Table(
+    "dataset.Warehouse",
+    metadata,
+    Column("warehouse", String),
+    Column("state", String),
+)
+
+detailed_inventory = Table(
+    "dataset.DetailedInventory",
+    metadata,
+    Column("product", String),
+    Column("quantity", Integer),
+    Column("supply_constrained", Boolean),
+    Column("comments", ARRAY(JSON)),
+    Column("specifications", JSON),
 )
--- tests/unit/test_sql_compiles.py
+++ tests/unit/test_sql_compiles.py
@@ -2,10 +2,10 @@ from datetime import timedelta
 from textwrap import dedent
 
 from pybigquery.sqlalchemy_bigquery import BigQueryDialect
-from sqlalchemy import insert, select, update
+from sqlalchemy import delete, insert, literal, select, text, update
 
 from pybigquery_merge_into.merge_clause import MergeInto, WhenMatched, WhenNotMatched
-from tests.conftest import source, target
+from tests.conftest import detailed_inventory, inventory, new_arrivals, source, target
 
 
 def test_when_matched():
@@ -84,3 +84,125 @@ def test_cte_in_source():
         """
 
     assert str(query.compile(dialect=BigQueryDialect())) == dedent(expected)
+
+
+# Check we can make the examples from https://cloud.google.com/bigquery/docs/reference/standard-sql/dml-syntax#merge_examples work
+# Note that the "expected" value might have be slightly reworked (indentation, extra AS, etc) but should still be semantically the same.
+# Known differences are:
+#   - extra AS before aliases
+#   - extra backticks (`) around identifiers
+#   - slightly different indentation
+#   - adds the INTO in "MERGE [INTO]"
+#   - always specifies "WHEN NOT MATCH [BY TARGET]"
+def test_compiles_example_1():
+    expected = """\
+        MERGE INTO `dataset.DetailedInventory` AS `T`
+        USING `dataset.Inventory` AS `S`
+        ON `T`.`product` = `S`.`product`
+        WHEN NOT MATCHED BY TARGET AND `T`.`quantity` < 20 THEN 
+        \tINSERT (`product`, `quantity`, `supply_constrained`, `comments`) VALUES (`S`.`product`, `S`.`quantity`, true, ARRAY<STRUCT<created DATE, comment STRING>>[(DATE('2016-01-01'), 'comment1')])
+        WHEN NOT MATCHED BY TARGET THEN 
+        \tINSERT (`product`, `quantity`, `supply_constrained`) VALUES (`S`.`product`, `S`.`quantity`, false)
+        """
+
+    T = detailed_inventory.alias("T")
+    S = inventory.alias("S")
+
+    query = MergeInto(
+        target=T,
+        source=S,
+        onclause=T.c.product == S.c.product,
+        when_clauses=[
+            WhenNotMatched(
+                insert(T).values({
+                    T.c.product: S.c.product,
+                    T.c.quantity: S.c.quantity,
+                    T.c.supply_constrained: literal(True),
+                    T.c.comments: text("ARRAY<STRUCT<created DATE, comment STRING>>[(DATE('2016-01-01'), 'comment1')]")  # can't figure this one out without cheating a bit
+                }),
+                condition=T.c.quantity < 20
+            ),
+            WhenNotMatched(
+                insert(T).values({
+                    T.c.product: S.c.product,
+                    T.c.quantity: S.c.quantity,
+                    T.c.supply_constrained: literal(False),
+                }),
+            )
+        ]
+    )
+
+    assert str(query.compile(dialect=BigQueryDialect(), compile_kwargs={'literal_binds': True})) == dedent(expected)
+
+
+def test_example_2():
+    expected = """\
+        MERGE INTO `dataset.Inventory` AS `T`
+        USING `dataset.NewArrivals` AS `S`
+        ON `T`.`product` = `S`.`product`
+        WHEN MATCHED THEN 
+        \tUPDATE  SET `quantity`=(`T`.`quantity` + `S`.`quantity`)
+        WHEN NOT MATCHED BY TARGET THEN 
+        \tINSERT (`product`, `quantity`) VALUES (`S`.`product`, `S`.`quantity`)
+        """
+
+    T = inventory.alias("T")
+    S = new_arrivals.alias("S")
+
+    query = MergeInto(
+        target=T,
+        source=S,
+        onclause=T.c.product == S.c.product,
+        when_clauses=[
+            WhenMatched(
+                update(T).values({
+                    T.c.quantity: T.c.quantity + S.c.quantity,
+                }),
+            ),
+            WhenNotMatched(
+                insert(T).values({
+                    T.c.product: S.c.product,
+                    T.c.quantity: S.c.quantity,
+                }),
+            )
+        ]
+    )
+
+    assert str(query.compile(dialect=BigQueryDialect(), compile_kwargs={'literal_binds': True})) == dedent(expected)
+
+
+# Note that the subquery is different because querying `*` is annoying to do using SQLAlchemy
+def test_example_3():
+    expected = """\
+        MERGE INTO `dataset.NewArrivals` AS `T`
+        USING (SELECT `dataset.NewArrivals`.`product` AS `product` 
+        FROM `dataset.NewArrivals` 
+        WHERE `dataset.NewArrivals`.`warehouse` != 'warehouse #2') AS `S`
+        ON `T`.`product` = `S`.`product`
+        WHEN MATCHED AND `T`.`warehouse` = 'warehouse #1' THEN 
+        \tUPDATE  SET `quantity`=(`T`.`quantity` + 20)
+        WHEN MATCHED THEN 
+        \tDELETE
+        """
+
+    T = new_arrivals.alias("T")
+    S = select([new_arrivals.c.product]).where(new_arrivals.c.warehouse != "warehouse #2").alias("S")
+
+    query = MergeInto(
+        target=T,
+        source=S,
+        onclause=T.c.product == S.c.product,
+        when_clauses=[
+            WhenMatched(
+                update(T).values({
+                    T.c.quantity: T.c.quantity + 20,
+                }),
+                condition=T.c.warehouse == "warehouse #1"
+            ),
+            WhenMatched(
+                delete(T)
+            )
+        ]
+    )
+
+    assert str(query.compile(dialect=BigQueryDialect(), compile_kwargs={'literal_binds': True})) == dedent(expected)
